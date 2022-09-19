7e5d666bd3f6a73875845e4ffd93504144a6dbc5
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-09-19 13:21:30 -0700

    wire up basic animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './styles/App.scss';
 
 function App() {
+
+  useEffect(() => {
+
+    function wrapChars(str, i) {
+      console.log("i", i)
+      return `<span class="text" style="animation-delay: calc(5 * ${i}0ms);">${str}</span>`;
+    }
+
+    const animate = () => {
+      const allTextElement = document.querySelector(".text-animation__text");
+      const allText = document.querySelector(".text-animation__text").innerText;
+      
+      const arrayOfChars = [];
+      for (let i = 0; i < allText.length; i++) {
+        arrayOfChars.push(wrapChars(allText.charAt(i), i))
+      }
+
+      allTextElement.innerHTML = arrayOfChars.join('');
+      console.log("allTextElement", allTextElement);
+    }
+    animate();
+
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='text-animation'>
+        <h1 className='text-animation__text'>WIPINGTEXT</h1>
+      </div>
   );
 }
 
